b7cec3a0704c4ba2d773542502bf24df7fb505ec
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2019-09-30 14:47:48 +0200

    Implemented multiple changes

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
 # Core System
 [![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](./LICENSE.txt)
+
 This repository contains scripts to set up an working client system, maintain it and to save the data on an USB stick.
-It's adapted to the needs of Kevin Veen-Birkenbach.
+It's adapted to the needs of Kevin Veen-Birkenbach aka. Frantz.
+## Requirements
+This script is optimized for a [Manjaro Linux](https://manjaro.org) with [GNOME desktop](https://www.gnome.org/?). 
+Specific system requirements are described in the [.travis file](./.travis).
+
+## Functions
+### System Setup
+To setup the customized software on a system you have to execute:
+```bash
+bash ./scripts/system-setup.sh
+```
+### Import Data
+To import configuration files from the system you have to execute:
+```bash
+bash ./scripts/import-data-from-system.sh
+```
+### Export Data
+To export configuration files to the system you have to execute:
+```bash
+bash ./scripts/export-data-to-system.sh
+```
+### Decrypt Data
+To decrypt the data you have to execute:
+```bash
+bash ./scripts/decrypt-data.sh
+```
+### Encrypt Data
+To encrypt the data you have to execute:
+```bash
+bash ./scripts/encrypt-data.sh
+```
 ## License
 The ["GNU GENERAL PUBLIC LICENSE Version 3"](./LICENSE.txt) applies to this project.
--- scripts/system-setup.sh
+++ scripts/system-setup.sh
@@ -8,6 +8,11 @@ echo "Synchronizing programing languages..."
 sudo pacman --needed -S jdk11-openjdk python php
 echo "Synchronizing administration tools..."
 sudo pacman --needed -S htop tree git base-devel yay make gcc cmake
+echo "Synchronizing gui tools..."
+sudo pacman --needed -S gnome-shell-extensions
+echo "Install NASA picture of the day GNOME extension..."
+git clone https://github.com/Elinvention/gnome-shell-extension-nasa-apod.git $HOME/.local/share/gnome-shell/extensions/nasa_apod@elinvention.ovh
+gnome-shell-extension-tool -e nasa_apod@elinvention.ovh
 echo "Synchronizing office tools..."
 sudo pacman --needed -S chromium gimp
 echo "Synchronizing communication tools..."
@@ -44,9 +49,11 @@ sudo pacman --needed -S docker
 echo "Synchronizing orchestration tools..."
 sudo pacman --needed -S ansible
 echo "Add current user($USER) to user group docker..."
-usermod -a -G docker $USER
+sudo usermod -a -G docker $USER
+echo "Enable docker service..."
+sudo systemctl enable docker --now
 echo "Synchronizing games..."
 sudo pacman --needed -S 0ad warzone2100
-#yay pacman --needed -S emulationstation
+yay -S emulationstation
 echo "More game recomendations you will find here: https://wiki.archlinux.org/index.php/List_of_games..."
 echo "More software recomendations you will find here: https://wiki.archlinux.org/index.php/list_of_applications"
