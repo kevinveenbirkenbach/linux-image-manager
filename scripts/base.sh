bad1ecccb09e0fa337b8a8c88c6320535201d0e3
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2019-10-01 08:56:34 +0200

    Refactored variables

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This script contains the global program variables and functions
 # @author Kevin Veen-Birkenbach [aka. Frantz]
-ENCRYPTED=$(readlink -f "$(dirname "$(readlink -f "${0}")")/../.encrypted");
-DECRYPTED=$(readlink -f "$(dirname "$(readlink -f "${0}")")/../data");
+REPOSITORY_PATH="$(dirname "$(readlink -f "${0}")")"
+ENCRYPTED=$(readlink -f "$REPOSITORY_PATH/../.encrypted");
+DECRYPTED=$(readlink -f "$REPOSITORY_PATH/../data");
