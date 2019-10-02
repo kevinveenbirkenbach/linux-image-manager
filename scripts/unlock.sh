8ec28266b8929f597241f07aa6c8b637d34ec86e
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2019-10-02 16:51:08 +0200

    Added creation of decrypted folder just if the folder doesn't exist

--- scripts/unlock.sh
+++ scripts/unlock.sh
@@ -6,7 +6,10 @@
 # shellcheck source=/dev/null # Deactivate SC1090
 source "$(dirname "$(readlink -f "${0}")")/base.sh"
 echo "Unlocking directory $DECRYPTED_PATH..."
-echo "Creating directory $DECRYPTED_PATH..."
-mkdir "$DECRYPTED_PATH"
+if [ ! -d "$DECRYPTED_PATH" ]
+  then
+    echo "Creating directory $DECRYPTED_PATH..."
+    mkdir "$DECRYPTED_PATH"
+fi
 echo "Encrypting directory $DECRYPTED_PATH to $DECRYPTED_PATH..."
 encfs "$ENCRYPTED_PATH" "$DECRYPTED_PATH" && echo "ATTENTION: DATA IS NOW DECRYPTED!"
