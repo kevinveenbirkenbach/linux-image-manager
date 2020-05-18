7028ca48b7a95ef3539d650584734dc14277c860
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2020-05-18 21:29:23 +0200

    Added permission setter for import

--- scripts/data/export-to-system.sh
+++ scripts/data/export-to-system.sh
@@ -4,3 +4,8 @@
 # shellcheck source=/dev/null # Deactivate SC1090
 source "$(dirname "$(readlink -f "${0}")")/../base.sh" || (echo "Loading base.sh failed." && exit 1)
 bash "$SCRIPT_PATH""data/import-from-system.sh" reverse
+info "Setting right permissions for importet files..."
+chown -R $USER:$USER ~
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa.pub
