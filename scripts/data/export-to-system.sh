7057b753499d4f7cb9407b0e4e0328c71fde25ee
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2020-05-19 13:51:31 +0200

    Solved permission bug

--- scripts/data/export-to-system.sh
+++ scripts/data/export-to-system.sh
@@ -6,6 +6,6 @@ source "$(dirname "$(readlink -f "${0}")")/../base.sh" || (echo "Loading base.sh
 bash "$SCRIPT_PATH""data/import-from-system.sh" reverse
 info "Setting right permissions for importet files..."
 chown -R $USER:$USER ~
-chmod 700 ~/.ssh
+chmod -R 700 ~/.ssh
 chmod 600 ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa.pub
