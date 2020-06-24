31c7950547782d43b95c4dcde49ca840fd80dab9
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2020-06-24 09:31:57 +0200

    Optimized right permission setting

--- scripts/data/export-to-system.sh
+++ scripts/data/export-to-system.sh
@@ -6,8 +6,7 @@
 source "$(dirname "$(readlink -f "${0}")")/../base.sh" || (echo "Loading base.sh failed." && exit 1)
 bash "$SCRIPT_PATH""data/import-from-system.sh" reverse
 info "Setting right permissions for importet files..." &&
-chown -R "$USER":"$USER" ~ &&
 chmod -R 700 ~/.ssh &&
 chmod 600 ~/.ssh/id_rsa &&
-chmod 600 ~/.ssh/id_rsa.pub ||
-error
+chmod 600 ~/.ssh/id_rsa.pub || error "Failed to set correct ssh permissions"
+chown -R "$USER":"$USER" ~ || warning "Not all files could be owned by user \"$USER\"..."
