2d95f1caefe33087a4e0bf17d2b116e63f3f58e0
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2020-09-30 16:43:59 +0200

    Deleted debuging option

--- scripts/image/setup.sh
+++ scripts/image/setup.sh
@@ -434,7 +434,7 @@ if [ "$encrypt_system" == "y" ]
     boot_txt_rescue_path="$boot_txt_path$rescue_suffix"
     boot_txt_delete_line=$(echo "part uuid \${devtype} \${devnum}:2 uuid" | sed -e 's/[]\/$*.^[]/\\&/g')
     boot_txt_setenv_origin=$(echo "setenv bootargs console=ttyS1,115200 console=tty0 root=PARTUUID=\${uuid} rw rootwait smsc95xx.macaddr=\"\${usbethaddr}\"" | sed -e 's/[]\/$*.^[]/\\&/g')
-    boot_txt_setenv_replace=$(echo "setenv bootargs earlyprintk console=ttyS1,115200 console=tty0 ip=::::$target_hostname:eth0:dhcp cryptdevice=$encrypted_partition_path:root root=$root_mapper_path rw rootwait smsc95xx.macaddr=\"\${usbethaddr}\""| sed -e 's/[\/&]/\\&/g')
+    boot_txt_setenv_replace=$(echo "setenv bootargs console=ttyS1,115200 console=tty0 ip=::::$target_hostname:eth0:dhcp cryptdevice=$encrypted_partition_path:root root=$root_mapper_path rw rootwait smsc95xx.macaddr=\"\${usbethaddr}\""| sed -e 's/[\/&]/\\&/g')
     info "Setup encryption..." &&
     question "Type in encryption password: " && read -r luks_password
     question "Repeat encryption password:" && read -r luks_password_repeat
