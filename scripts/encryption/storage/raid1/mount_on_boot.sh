bf49689723dae313e451f841124468f4c99556d8
Kevin Veen-Birkenbach <kevin@veen.world>
2020-12-20 22:28:07 +0100

    Solved variable naming bug

--- scripts/encryption/storage/raid1/mount_on_boot.sh
+++ scripts/encryption/storage/raid1/mount_on_boot.sh
@@ -4,8 +4,8 @@
 source "$(dirname "$(readlink -f "${0}")")/base.sh" || (echo "Loading base.sh failed." && exit 1)
 info "Automount raid1 encrypted storages..." &&
 set_raid1_devices_mount_partition_and_mapper_paths &&
-create_luks_key_and_update_cryptab "$mapper_name_1" "$partition_path_1" &&
-create_luks_key_and_update_cryptab "$mapper_name_2" "$partition_path_2" &&
+create_luks_key_and_update_cryptab "$mapper_name_1" "$device_path_1" &&
+create_luks_key_and_update_cryptab "$mapper_name_2" "$device_path_2" &&
 update_fstab "$mapper_path_1" "$mount_path_1" &&
 success "Installation finished. Please restart :)" ||
 error
