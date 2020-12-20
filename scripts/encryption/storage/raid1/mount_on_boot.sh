35d67a63c8993940175cd0b7a60025c7a9ea791f
Kevin Veen-Birkenbach <kevin@veen.world>
2020-12-20 22:26:46 +0100

    Solved bug

--- scripts/encryption/storage/raid1/mount_on_boot.sh
+++ scripts/encryption/storage/raid1/mount_on_boot.sh
@@ -3,7 +3,7 @@
 # shellcheck disable=SC2154  # Referenced but not assigned
 source "$(dirname "$(readlink -f "${0}")")/base.sh" || (echo "Loading base.sh failed." && exit 1)
 info "Automount raid1 encrypted storages..." &&
-set_device_mount_partition_and_mapper_paths &&
+set_raid1_devices_mount_partition_and_mapper_paths &&
 create_luks_key_and_update_cryptab "$mapper_name_1" "$partition_path_1" &&
 create_luks_key_and_update_cryptab "$mapper_name_2" "$partition_path_2" &&
 update_fstab "$mapper_path_1" "$mount_path_1" &&
