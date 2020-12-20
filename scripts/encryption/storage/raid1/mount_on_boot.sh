0b0e34ac04c6f31433ad2ca1404073007527f58e
Kevin Veen-Birkenbach <kevin@veen.world>
2020-12-20 22:23:52 +0100

    Solved bug

--- scripts/encryption/storage/raid1/mount_on_boot.sh
+++ scripts/encryption/storage/raid1/mount_on_boot.sh
@@ -2,8 +2,10 @@
 # shellcheck disable=SC1090  # Can't follow non-constant source. Use a directive to specify location.
 # shellcheck disable=SC2154  # Referenced but not assigned
 source "$(dirname "$(readlink -f "${0}")")/base.sh" || (echo "Loading base.sh failed." && exit 1)
-info "Automount raid1 encrypted storages..."
-create_luks_key_and_update_cryptab "$mapper_name_1" "$partition_path_1"
-create_luks_key_and_update_cryptab "$mapper_name_2" "$partition_path_2"
-update_fstab "$mapper_path_1" "$mount_path_1"
-success "Installation finished. Please restart :)"
+info "Automount raid1 encrypted storages..." &&
+set_device_mount_partition_and_mapper_paths &&
+create_luks_key_and_update_cryptab "$mapper_name_1" "$partition_path_1" &&
+create_luks_key_and_update_cryptab "$mapper_name_2" "$partition_path_2" &&
+update_fstab "$mapper_path_1" "$mount_path_1" &&
+success "Installation finished. Please restart :)" ||
+error
