ab1316b437202dc0181701aa45a8676fe3ac6c6c
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2021-12-08 13:05:19 +0100

    Deleted gnome configuration

--- scripts/client/setup.sh
+++ scripts/client/setup.sh
@@ -145,7 +145,6 @@ install_gnome_extension(){
 
 if [ "$DESKTOP_SESSION" == "gnome" ]; then
   info "Synchronizing gnome tools..." &&
-  sudo pacman -S "$(get_packages 'client/pacman/gnome')" &&
   install_yay_packages_if_needed "$(get_packages "client/yay/gnome")" || error "Syncronisation failed."
 	info "Setting up gnome dash favourites..." &&
 	gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop',
