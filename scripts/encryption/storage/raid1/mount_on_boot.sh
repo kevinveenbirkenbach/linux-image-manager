72c4d95606b7ade55d088c6bb879f7c8ed3f3e84
Kevin Veen-Birkenbach <kevin@veen.world>
2025-02-15 12:03:51 +0100

    Added overview about LUKS encrypted storage devices

--- scripts/encryption/storage/raid1/mount_on_boot.sh
+++ scripts/encryption/storage/raid1/mount_on_boot.sh
@@ -3,7 +3,13 @@
 # shellcheck disable=SC2154  # Referenced but not assigned
 # shellcheck disable=SC2015 #Deactivate bool hint
 source "$(dirname "$(readlink -f "${0}")")/base.sh" || (echo "Loading base.sh failed." && exit 1)
-info "Automount raid1 encrypted storages..." &&
+info "Activate Automount raid1 encrypted storages..." &&
+echo ""
+for dev in $(lsblk -dno NAME); do
+    if sudo cryptsetup isLuks /dev/$dev 2>/dev/null; then
+        info "/dev/$dev is a LUKS encrypted storage device."
+    fi
+done
 set_raid1_devices_mount_partition_and_mapper_paths &&
 create_luks_key_and_update_cryptab "$mapper_name_1" "$device_path_1" &&
 info "Creating mount folder unter \"$mount_path_1\"..." &&
